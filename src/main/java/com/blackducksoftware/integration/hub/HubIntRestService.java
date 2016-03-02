ef1c01b438de07fb7de5cc79626b30f0342f72fb
jrichard <jrichard@localhost>
2016-03-02 14:22:03 -0500

    IJH-148 Adding new class to do the polling in the hub-ci

--- src/main/java/com/blackducksoftware/integration/hub/HubIntRestService.java
+++ src/main/java/com/blackducksoftware/integration/hub/HubIntRestService.java
@@ -16,11 +16,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
 
 import org.apache.commons.lang3.StringUtils;
-import org.joda.time.DateTime;
 import org.restlet.Context;
 import org.restlet.Response;
 import org.restlet.data.ChallengeRequest;
@@ -51,10 +49,8 @@ import com.blackducksoftware.integration.hub.response.VersionComparison;
 import com.blackducksoftware.integration.hub.response.mapping.AssetReferenceItem;
 import com.blackducksoftware.integration.hub.response.mapping.EntityItem;
 import com.blackducksoftware.integration.hub.response.mapping.EntityTypeEnum;
-import com.blackducksoftware.integration.hub.response.mapping.ScanHistoryItem;
 import com.blackducksoftware.integration.hub.response.mapping.ScanLocationItem;
 import com.blackducksoftware.integration.hub.response.mapping.ScanLocationResults;
-import com.blackducksoftware.integration.hub.response.mapping.ScanStatus;
 import com.blackducksoftware.integration.suite.sdk.logging.IntLogger;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -892,73 +888,6 @@ public class HubIntRestService {
             throw new BDRestException("There was a problem comparing the specified version to the version of the Hub server. Error Code: " + responseCode,
                     resource);
         }
-    }
-
-    /**
-     * Check the code locations with the host specified and the paths provided. Check the history for the scan history
-     * that falls between the times provided, if the status of that scan history for all code locations is complete then
-     * the bom is up to date with these scan results. Otherwise we try again after 10 sec, and we keep trying until it
-     * is up to date or until we hit the maximum wait time.
-     * If we find a scan history object that has status cancelled or an error type then we throw an exception.
-     *
-     *
-     * @param timeBeforeScan
-     *            DateTime before the Cli was run
-     * @param timeAfterScan
-     *            DateTime after the Cli was run
-     * @param hostname
-     *            String hostname where the Cli was run
-     * @param scanTargets
-     *            List<<String>> the target paths that were scanned
-     * @param maximumWait
-     *            long, maximum time to wait for the Bom to be updated completely
-     * @return True if the bom has been updated with the code locations from this scan
-     * @throws InterruptedException
-     * @throws BDRestException
-     * @throws HubIntegrationException
-     * @throws URISyntaxException
-     * @throws IOException
-     */
-    public boolean isBomUpToDate(DateTime timeBeforeScan, DateTime timeAfterScan, String hostname, List<String>
-            scanTargets, long maximumWait) throws InterruptedException, BDRestException, HubIntegrationException, URISyntaxException, IOException {
-        long startTime = System.currentTimeMillis();
-        long elapsedTime = 0;
-        while (elapsedTime < maximumWait) {
-            // logger.trace("CHECKING CODE LOCATIONS");
-            List<ScanLocationItem> scanLocationsToCheck = getScanLocations(hostname, scanTargets);
-            boolean upToDate = true;
-            for (ScanLocationItem currentCodeLocation : scanLocationsToCheck) {
-                for (ScanHistoryItem currentScanHistory : currentCodeLocation.getScanList()) {
-                    DateTime scanHistoryCreationTime = currentScanHistory.getCreatedOnTime();
-                    if (scanHistoryCreationTime != null && scanHistoryCreationTime.isAfter(timeBeforeScan) && scanHistoryCreationTime.isBefore(timeAfterScan)) {
-                        // This scan history Item came from the scan we executed
-                        if (ScanStatus.isFinishedStatus(currentScanHistory.getStatus())) {
-                            if (ScanStatus.isErrorStatus(currentScanHistory.getStatus())) {
-                                throw new HubIntegrationException("There was a problem with one of the code locations. Error Status : "
-                                        + currentScanHistory.getStatus().name());
-                            }
-                        } else {
-                            // The code location is still updating or matching, etc.
-                            upToDate = false;
-                        }
-                    } else {
-                        // This scan history Item did not come from the scan we executed
-                        continue;
-                    }
-                }
-            }
-            if (upToDate) {
-                // The code locations are all finished, so we know the bom has been updated with our scan results
-                // So we break out of this loop
-                return true;
-            }
-            // wait 10 seconds before checking the status's again
-            Thread.sleep(10000);
-            elapsedTime = System.currentTimeMillis() - startTime;
-        }
-        String formattedTime = String.format("%d minutes", TimeUnit.MILLISECONDS.toMinutes(maximumWait));
-        throw new HubIntegrationException("The Bom has not finished updating from the scan within the specified wait time : " + formattedTime);
-
     }
 
     /**
