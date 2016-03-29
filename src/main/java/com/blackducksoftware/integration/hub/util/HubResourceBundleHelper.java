e9fc14b0184bb11b45ebd69478f016211c7f79b3
Eric Kerwin <ekerwin@blackducksoftware.com>
2016-03-29 10:48:42 -0400

    making Paulo's changes

--- src/main/java/com/blackducksoftware/integration/hub/util/HubResourceBundleHelper.java
+++ src/main/java/com/blackducksoftware/integration/hub/util/HubResourceBundleHelper.java
@@ -1,56 +1,37 @@
 package com.blackducksoftware.integration.hub.util;
 
-import java.util.Enumeration;
 import java.util.Locale;
 import java.util.ResourceBundle;
-
-import org.apache.commons.lang3.StringUtils;
 
 public class HubResourceBundleHelper {
     private static final String RESOURCE_BASE_NAME = "com.blackducksoftware.integration.hub.resources.HubResources";
 
-    public static void main(String[] args) {
-        HubResourceBundleHelper helper = new HubResourceBundleHelper();
-        helper.generateQaResources();
-    }
+    private Locale locale = Locale.US;
 
-    public String getRiskReportKey(String key, Locale locale) {
-        return getBundle(locale).getString("hub.riskreport." + key);
-    }
+    private String keyPrefix;
 
-    public String getRiskReportKey(String key) {
-        return getBundle().getString("hub.riskreport." + key);
-    }
-
-    private ResourceBundle getBundle() {
-        return ResourceBundle.getBundle(RESOURCE_BASE_NAME);
-    }
-
-    private ResourceBundle getBundle(Locale locale) {
-        return ResourceBundle.getBundle(RESOURCE_BASE_NAME, locale);
-    }
-
-    private void generateQaResources() {
-        ResourceBundle bundle = getBundle(Locale.US);
-        Enumeration<String> keys = bundle.getKeys();
-        StringBuilder content = new StringBuilder();
-        while (keys.hasMoreElements()) {
-            String key = keys.nextElement();
-            String message = bundle.getString(key);
-
-            StringBuilder revisedMessage = new StringBuilder();
-            revisedMessage.append("!!**");
-            for (char c : message.toCharArray()) {
-                revisedMessage.append(c);
-                if (StringUtils.isAlphanumeric(String.valueOf(c)) && message.length() > 3) {
-                    revisedMessage.append(c);
-                }
-            }
-            revisedMessage.append("**!!");
-            content.append(key + "=" + revisedMessage.toString() + "\n");
+    public String getString(String key) {
+        if (null != keyPrefix) {
+            key = keyPrefix + "." + key;
         }
 
-        System.out.println(content);
+        return ResourceBundle.getBundle(RESOURCE_BASE_NAME, locale).getString(key);
+    }
+
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public void setLocale(Locale locale) {
+        this.locale = locale;
+    }
+
+    public String getKeyPrefix() {
+        return keyPrefix;
+    }
+
+    public void setKeyPrefix(String keyPrefix) {
+        this.keyPrefix = keyPrefix;
     }
 
 }
--- src/test/java/com/blackducksoftware/integration/hub/util/HubResourceBundleHelperTest.java
+++ src/test/java/com/blackducksoftware/integration/hub/util/HubResourceBundleHelperTest.java
@@ -8,24 +8,28 @@ import org.junit.Test;
 
 public class HubResourceBundleHelperTest {
     @Test
-    public void testGettingRiskReportKeyWithDefaultLocale() {
-        HubResourceBundleHelper helper = new HubResourceBundleHelper();
-        String high = helper.getRiskReportKey("entry.high");
-        assertEquals("High", high);
-    }
-
-    @Test
     public void testGettingRiskReportKeyWithUSLocale() {
         HubResourceBundleHelper helper = new HubResourceBundleHelper();
-        String high = helper.getRiskReportKey("entry.high", Locale.US);
+        helper.setKeyPrefix("hub.riskreport");
+        String high = helper.getString("entry.high");
         assertEquals("High", high);
     }
 
     @Test
     public void testGettingRiskReportKeyForQaLocale() {
         HubResourceBundleHelper helper = new HubResourceBundleHelper();
-        String high = helper.getRiskReportKey("entry.high", new Locale("qa", "QA"));
+        helper.setKeyPrefix("hub.riskreport");
+        helper.setLocale(new Locale("qa", "QA"));
+        String high = helper.getString("entry.high");
         assertEquals("!!**HHiigghh**!!", high);
+    }
+
+    @Test
+    public void testGettingRiskReportKeyWithoutPrefix() {
+        HubResourceBundleHelper helper = new HubResourceBundleHelper();
+        helper.setLocale(new Locale("qa", "QA"));
+        String high = helper.getString("hub.riskreport.entry.medium");
+        assertEquals("!!**MMeeddiiuumm**!!", high);
     }
 
 }
