4ec8190382274744bd04df8433a989b4041e2927
jrichard <jrichard@blackducksoftware.com>
2016-03-30 15:27:10 -0400

    IJH-158 fixing the serialization issue Dohwee discovered

--- src/main/java/com/blackducksoftware/integration/hub/report/api/HubReportGenerationInfo.java
+++ src/main/java/com/blackducksoftware/integration/hub/report/api/HubReportGenerationInfo.java
@@ -1,100 +1,101 @@
 package com.blackducksoftware.integration.hub.report.api;
 
+import java.io.Serializable;
 import java.util.List;
 
 import org.joda.time.DateTime;
 
 import com.blackducksoftware.integration.hub.HubIntRestService;
 
-public class HubReportGenerationInfo {
-	private HubIntRestService service;
+public class HubReportGenerationInfo implements Serializable {
+    private HubIntRestService service;
 
-	private String projectId;
+    private String projectId;
 
-	private String versionId;
+    private String versionId;
 
-	private String hostname;
+    private String hostname;
 
-	private List<String> scanTargets;
+    private List<String> scanTargets;
 
-	private long maximumWaitTime;
+    private long maximumWaitTime;
 
-	private DateTime beforeScanTime;
+    private DateTime beforeScanTime;
 
-	private DateTime afterScanTime;
+    private DateTime afterScanTime;
 
-	private String scanStatusDirectory;
+    private String scanStatusDirectory;
 
-	public HubIntRestService getService() {
-		return service;
-	}
+    public HubIntRestService getService() {
+        return service;
+    }
 
-	public String getProjectId() {
-		return projectId;
-	}
+    public String getProjectId() {
+        return projectId;
+    }
 
-	public String getVersionId() {
-		return versionId;
-	}
+    public String getVersionId() {
+        return versionId;
+    }
 
-	public String getHostname() {
-		return hostname;
-	}
+    public String getHostname() {
+        return hostname;
+    }
 
-	public List<String> getScanTargets() {
-		return scanTargets;
-	}
+    public List<String> getScanTargets() {
+        return scanTargets;
+    }
 
-	public long getMaximumWaitTime() {
-		return maximumWaitTime;
-	}
+    public long getMaximumWaitTime() {
+        return maximumWaitTime;
+    }
 
-	public DateTime getBeforeScanTime() {
-		return beforeScanTime;
-	}
+    public DateTime getBeforeScanTime() {
+        return beforeScanTime;
+    }
 
-	public DateTime getAfterScanTime() {
-		return afterScanTime;
-	}
+    public DateTime getAfterScanTime() {
+        return afterScanTime;
+    }
 
-	public void setService(final HubIntRestService service) {
-		this.service = service;
-	}
+    public void setService(final HubIntRestService service) {
+        this.service = service;
+    }
 
-	public void setProjectId(final String projectId) {
-		this.projectId = projectId;
-	}
+    public void setProjectId(final String projectId) {
+        this.projectId = projectId;
+    }
 
-	public void setVersionId(final String versionId) {
-		this.versionId = versionId;
-	}
+    public void setVersionId(final String versionId) {
+        this.versionId = versionId;
+    }
 
-	public void setHostname(final String hostname) {
-		this.hostname = hostname;
-	}
+    public void setHostname(final String hostname) {
+        this.hostname = hostname;
+    }
 
-	public void setScanTargets(final List<String> scanTargets) {
-		this.scanTargets = scanTargets;
-	}
+    public void setScanTargets(final List<String> scanTargets) {
+        this.scanTargets = scanTargets;
+    }
 
-	public void setMaximumWaitTime(final long maximumWaitTime) {
-		this.maximumWaitTime = maximumWaitTime;
-	}
+    public void setMaximumWaitTime(final long maximumWaitTime) {
+        this.maximumWaitTime = maximumWaitTime;
+    }
 
-	public void setBeforeScanTime(final DateTime beforeScanTime) {
-		this.beforeScanTime = beforeScanTime;
-	}
+    public void setBeforeScanTime(final DateTime beforeScanTime) {
+        this.beforeScanTime = beforeScanTime;
+    }
 
-	public void setAfterScanTime(final DateTime afterScanTime) {
-		this.afterScanTime = afterScanTime;
-	}
+    public void setAfterScanTime(final DateTime afterScanTime) {
+        this.afterScanTime = afterScanTime;
+    }
 
-	public String getScanStatusDirectory() {
-		return scanStatusDirectory;
-	}
+    public String getScanStatusDirectory() {
+        return scanStatusDirectory;
+    }
 
-	public void setScanStatusDirectory(final String scanStatusDirectory) {
-		this.scanStatusDirectory = scanStatusDirectory;
-	}
+    public void setScanStatusDirectory(final String scanStatusDirectory) {
+        this.scanStatusDirectory = scanStatusDirectory;
+    }
 
 }
