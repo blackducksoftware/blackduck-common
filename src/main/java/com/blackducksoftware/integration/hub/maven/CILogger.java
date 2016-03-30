22270feb7fd2e8b2948f8117ed590ef483f2926e
jrichard <jrichard@blackducksoftware.com>
2016-03-30 10:47:09 -0400

    Preparing for open source

--- src/main/java/com/blackducksoftware/integration/hub/maven/CILogger.java
+++ src/main/java/com/blackducksoftware/integration/hub/maven/CILogger.java
@@ -14,121 +14,121 @@ import com.blackducksoftware.integration.suite.sdk.logging.LogLevel;
  */
 public class CILogger {
 
-    private Logger logger;
+	private Logger logger;
 
-    private IntLogger intLogger;
+	private IntLogger intLogger;
 
-    protected CILogger(IntLogger intLogger, Logger logger) {
-        this.intLogger = intLogger;
-        this.logger = logger;
-    }
+	protected CILogger(final IntLogger intLogger, final Logger logger) {
+		this.intLogger = intLogger;
+		this.logger = logger;
+	}
 
-    public Logger getLogger() {
-        return logger;
-    }
+	public Logger getLogger() {
+		return logger;
+	}
 
-    public void setLogger(Logger logger) {
-        this.logger = logger;
-    }
+	public void setLogger(final Logger logger) {
+		this.logger = logger;
+	}
 
-    public IntLogger getIntLogger() {
-        return intLogger;
-    }
+	public IntLogger getIntLogger() {
+		return intLogger;
+	}
 
-    public void setIntLogger(IntLogger intLogger) {
-        this.intLogger = intLogger;
-    }
+	public void setIntLogger(final IntLogger intLogger) {
+		this.intLogger = intLogger;
+	}
 
-    public void info(String txt) {
-        if (intLogger != null) {
-            intLogger.info(txt);
-        } else if (logger != null) {
-            logger.info(txt);
-        }
+	public void info(final String txt) {
+		if (intLogger != null) {
+			intLogger.info(txt);
+		} else if (logger != null) {
+			logger.info(txt);
+		}
 
-    }
+	}
 
-    public void error(Throwable e) {
-        if (intLogger != null) {
-            intLogger.error(e);
-        } else if (logger != null) {
-            logger.error(e.getMessage(), e);
-        }
+	public void error(final Throwable e) {
+		if (intLogger != null) {
+			intLogger.error(e);
+		} else if (logger != null) {
+			logger.error(e.getMessage(), e);
+		}
 
-    }
+	}
 
-    public void error(String txt, Throwable e) {
-        if (intLogger != null) {
-            intLogger.error(txt, e);
-        } else if (logger != null) {
-            logger.error(txt, e);
-        }
+	public void error(final String txt, final Throwable e) {
+		if (intLogger != null) {
+			intLogger.error(txt, e);
+		} else if (logger != null) {
+			logger.error(txt, e);
+		}
 
-    }
+	}
 
-    public void error(String txt) {
-        if (intLogger != null) {
-            intLogger.error(txt);
-        } else if (logger != null) {
-            logger.error(txt);
-        }
+	public void error(final String txt) {
+		if (intLogger != null) {
+			intLogger.error(txt);
+		} else if (logger != null) {
+			logger.error(txt);
+		}
 
-    }
+	}
 
-    public void warn(String txt) {
-        if (intLogger != null) {
-            intLogger.warn(txt);
-        } else if (logger != null) {
-            logger.warn(txt);
-        }
+	public void warn(final String txt) {
+		if (intLogger != null) {
+			intLogger.warn(txt);
+		} else if (logger != null) {
+			logger.warn(txt);
+		}
 
-    }
+	}
 
-    public void trace(String txt) {
-        if (intLogger != null) {
-            intLogger.trace(txt);
-        } else if (logger != null) {
-            logger.trace(txt);
-        }
+	public void trace(final String txt) {
+		if (intLogger != null) {
+			intLogger.trace(txt);
+		} else if (logger != null) {
+			logger.trace(txt);
+		}
 
-    }
+	}
 
-    public void trace(String txt, Throwable e) {
-        if (intLogger != null) {
-            intLogger.trace(txt, e);
-        } else if (logger != null) {
-            logger.trace(txt, e);
-        }
-    }
+	public void trace(final String txt, final Throwable e) {
+		if (intLogger != null) {
+			intLogger.trace(txt, e);
+		} else if (logger != null) {
+			logger.trace(txt, e);
+		}
+	}
 
-    public void debug(String txt) {
-        if (intLogger != null) {
-            intLogger.debug(txt);
-        } else if (logger != null) {
-            logger.debug(txt);
-        }
+	public void debug(final String txt) {
+		if (intLogger != null) {
+			intLogger.debug(txt);
+		} else if (logger != null) {
+			logger.debug(txt);
+		}
 
-    }
+	}
 
-    public void debug(String txt, Throwable e) {
-        if (intLogger != null) {
-            intLogger.debug(txt, e);
-        } else if (logger != null) {
-            logger.debug(txt, e);
-        }
+	public void debug(final String txt, final Throwable e) {
+		if (intLogger != null) {
+			intLogger.debug(txt, e);
+		} else if (logger != null) {
+			logger.debug(txt, e);
+		}
 
-    }
+	}
 
-    public void setLogLevel(LogLevel logLevel) {
-        if (intLogger != null) {
-            intLogger.setLogLevel(logLevel);
-        }
-    }
+	public void setLogLevel(final LogLevel logLevel) {
+		if (intLogger != null) {
+			intLogger.setLogLevel(logLevel);
+		}
+	}
 
-    public LogLevel getLogLevel() {
-        if (intLogger != null) {
-            return intLogger.getLogLevel();
-        }
-        return null;
-    }
+	public LogLevel getLogLevel() {
+		if (intLogger != null) {
+			return intLogger.getLogLevel();
+		}
+		return null;
+	}
 }
