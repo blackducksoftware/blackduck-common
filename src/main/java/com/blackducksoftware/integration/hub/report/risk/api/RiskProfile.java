e2ca5c930eca91812e97f37d4ec3d0252ead10c2
jrichard <jrichard@blackducksoftware.com>
2016-04-08 12:13:43 -0400

    BOSS-75 adding copyright to the source files

--- src/main/java/com/blackducksoftware/integration/hub/report/risk/api/RiskProfile.java
+++ src/main/java/com/blackducksoftware/integration/hub/report/risk/api/RiskProfile.java
@@ -1,14 +1,21 @@
-/*
- * Copyright (C) 2014 Black Duck Software Inc.
- * http://www.blackducksoftware.com/
- * All rights reserved.
+/*******************************************************************************
+ * Black Duck Software Suite SDK
+ * Copyright (C) 2016 Black Duck Software, Inc.
  *
- * This software is the confidential and proprietary information of
- * Black Duck Software ("Confidential Information"). You shall not
- * disclose such Confidential Information and shall use it only in
- * accordance with the terms of the license agreement you entered into
- * with Black Duck Software.
- */
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *******************************************************************************/
 package com.blackducksoftware.integration.hub.report.risk.api;
 
 /**
