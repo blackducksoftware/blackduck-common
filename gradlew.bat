b660575b6989364aa1069d8a1b67c2f2ddbbe84d
Eric Kerwin <ekerwin@blackducksoftware.com>
2016-04-22 15:55:43 -0400

    adding copyrights

--- gradlew.bat
+++ gradlew.bat
@@ -1,3 +1,21 @@
+@rem ***************************************************************************
+@rem Black Duck Software Suite SDK
+@rem Copyright (C) 2016 Black Duck Software, Inc.
+@rem
+@rem This library is free software; you can redistribute it and/or
+@rem modify it under the terms of the GNU Lesser General Public
+@rem License as published by the Free Software Foundation; either
+@rem version 2.1 of the License, or (at your option) any later version.
+@rem
+@rem This library is distributed in the hope that it will be useful,
+@rem but WITHOUT ANY WARRANTY; without even the implied warranty of
+@rem MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+@rem Lesser General Public License for more details.
+@rem
+@rem You should have received a copy of the GNU Lesser General Public
+@rem License along with this library; if not, write to the Free Software
+@rem Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+@rem ***************************************************************************
 @if "%DEBUG%" == "" @echo off
 @rem ##########################################################################
 @rem
