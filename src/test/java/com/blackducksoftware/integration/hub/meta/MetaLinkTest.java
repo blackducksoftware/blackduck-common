17b372e1ac6955114e0d4c9009674ce0594da9a7
jrichard <jrichard@blackducksoftware.com>
2016-05-26 12:08:35 -0400

    Fixing copyright headers

--- src/test/java/com/blackducksoftware/integration/hub/meta/MetaLinkTest.java
+++ src/test/java/com/blackducksoftware/integration/hub/meta/MetaLinkTest.java
@@ -1,6 +1,7 @@
 /*******************************************************************************
  * Copyright (C) 2016 Black Duck Software, Inc.
  * http://www.blackducksoftware.com/
+ *  
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements. See the NOTICE file
  * distributed with this work for additional information
@@ -8,7 +9,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
+ *  
  * http://www.apache.org/licenses/LICENSE-2.0
+ *  
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
