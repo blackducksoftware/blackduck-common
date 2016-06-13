d3f41a707084643cd4b6d81bd20e1c5897bbafaa
jrichard <jrichard@blackducksoftware.com>
2016-06-13 18:11:04 -0400

    Handling null basetype. Otherwise it fails if the json is missing the "type" field

--- src/main/java/com/blackducksoftware/integration/hub/item/HubItemsService.java
+++ src/main/java/com/blackducksoftware/integration/hub/item/HubItemsService.java
@@ -48,6 +48,19 @@ public class HubItemsService<T> {
 	 * @param requestListTypeToken
 	 *            A Gson type token used to convert each item to an object
 	 */
+	public HubItemsService(final RestConnection restConnection, final TypeToken<T> requestListTypeToken) {
+		this(restConnection, null, requestListTypeToken, null);
+	}
+
+	/**
+	 * Construct a service for a given type of list.
+	 *
+	 * @param restConnection
+	 * @param baseType
+	 *            The common parent class of all items.
+	 * @param requestListTypeToken
+	 *            A Gson type token used to convert each item to an object
+	 */
 	public HubItemsService(final RestConnection restConnection, final Class<T> baseType,
 			final TypeToken<T> requestListTypeToken) {
 		this(restConnection, baseType, requestListTypeToken, null);
@@ -71,16 +84,19 @@ public class HubItemsService<T> {
 		this.restConnection = restConnection;
 		this.requestListTypeToken = requestListTypeToken;
 
-		final RuntimeTypeAdapterFactory<T> modelClassTypeAdapter = RuntimeTypeAdapterFactory
-				.of(baseType, "type");
-		if (typeNameToSubclassMap != null) {
-			for (final String typeName : typeNameToSubclassMap.keySet()) {
-				modelClassTypeAdapter.registerSubtype(
-						typeNameToSubclassMap.get(typeName), typeName);
-			}
-		}
 		final GsonBuilder gsonBuilder = new GsonBuilder();
-		gsonBuilder.registerTypeAdapterFactory(modelClassTypeAdapter);
+
+		if (baseType != null) {
+			final RuntimeTypeAdapterFactory<T> modelClassTypeAdapter = RuntimeTypeAdapterFactory
+					.of(baseType, "type");
+			if (typeNameToSubclassMap != null) {
+				for (final String typeName : typeNameToSubclassMap.keySet()) {
+					modelClassTypeAdapter.registerSubtype(
+							typeNameToSubclassMap.get(typeName), typeName);
+				}
+			}
+			gsonBuilder.registerTypeAdapterFactory(modelClassTypeAdapter);
+		}
 		gson = gsonBuilder.setDateFormat(RestConnection.JSON_DATE_FORMAT)
 				.create();
 	}
