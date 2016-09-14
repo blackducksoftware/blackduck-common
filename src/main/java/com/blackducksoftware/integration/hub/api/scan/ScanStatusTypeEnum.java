18e9473939872437fc5199ec59fc4f52057e2cf2
Eric Kerwin <ekerwin@blackducksoftware.com>
2016-09-14 10:11:38 -0400

    using EnumSet instead of List

--- src/main/java/com/blackducksoftware/integration/hub/api/scan/ScanStatusTypeEnum.java
+++ src/main/java/com/blackducksoftware/integration/hub/api/scan/ScanStatusTypeEnum.java
@@ -1,7 +1,7 @@
 package com.blackducksoftware.integration.hub.api.scan;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.EnumSet;
+import java.util.Set;
 
 public enum ScanStatusTypeEnum {
 	UNSTARTED,
@@ -21,10 +21,10 @@ public enum ScanStatusTypeEnum {
 	ERROR_BUILDING_BOM,
 	ERROR;
 
-	private static final List<ScanStatusTypeEnum> PENDING_STATES = Arrays.asList(UNSTARTED, SCANNING, SAVING_SCAN_DATA,
+	private static final Set<ScanStatusTypeEnum> PENDING_STATES = EnumSet.of(UNSTARTED, SCANNING, SAVING_SCAN_DATA,
 			SCAN_DATA_SAVE_COMPLETE, REQUESTED_MATCH_JOB, MATCHING, BOM_VERSION_CHECK, BUILDING_BOM);
-	private static final List<ScanStatusTypeEnum> DONE_STATES = Arrays.asList(COMPLETE, CANCELLED, CLONED,
-			ERROR_SCANNING, ERROR_SAVING_SCAN_DATA, ERROR_MATCHING, ERROR_BUILDING_BOM, ERROR);
+	private static final Set<ScanStatusTypeEnum> DONE_STATES = EnumSet.of(COMPLETE, CANCELLED, CLONED, ERROR_SCANNING,
+			ERROR_SAVING_SCAN_DATA, ERROR_MATCHING, ERROR_BUILDING_BOM, ERROR);
 
 	public boolean isPending() {
 		return PENDING_STATES.contains(this);
