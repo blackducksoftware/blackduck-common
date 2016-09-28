6b2b0095320757e06890cb97bd49c38020e085d6
Eric Kerwin <ekerwin@blackducksoftware.com>
2016-09-28 11:52:25 -0400

    adding remaining services to factory

--- src/main/java/com/blackducksoftware/integration/hub/dataservices/DataServicesFactory.java
+++ src/main/java/com/blackducksoftware/integration/hub/dataservices/DataServicesFactory.java
@@ -2,10 +2,16 @@ package com.blackducksoftware.integration.hub.dataservices;
 
 import com.blackducksoftware.integration.hub.api.bom.BomImportRestService;
 import com.blackducksoftware.integration.hub.api.codelocation.CodeLocationRestService;
+import com.blackducksoftware.integration.hub.api.component.ComponentVersionRestService;
+import com.blackducksoftware.integration.hub.api.notification.NotificationRestService;
+import com.blackducksoftware.integration.hub.api.policy.PolicyRestService;
 import com.blackducksoftware.integration.hub.api.policy.PolicyStatusRestService;
 import com.blackducksoftware.integration.hub.api.project.ProjectRestService;
 import com.blackducksoftware.integration.hub.api.project.ProjectVersionRestService;
 import com.blackducksoftware.integration.hub.api.scan.ScanSummaryRestService;
+import com.blackducksoftware.integration.hub.api.user.UserRestService;
+import com.blackducksoftware.integration.hub.api.version.VersionBomPolicyRestService;
+import com.blackducksoftware.integration.hub.api.vulnerabilities.VulnerabilityRestService;
 import com.blackducksoftware.integration.hub.dataservices.notification.NotificationDataService;
 import com.blackducksoftware.integration.hub.dataservices.notification.items.PolicyNotificationFilter;
 import com.blackducksoftware.integration.hub.dataservices.policystatus.PolicyStatusDataService;
@@ -20,22 +26,34 @@ public class DataServicesFactory {
 	final Gson gson = new Gson();
 	final JsonParser jsonParser = new JsonParser();
 
+	private final BomImportRestService bomImportRestService;
+	private final CodeLocationRestService codeLocationRestService;
+	private final ComponentVersionRestService componentVersionRestService;
+	private final NotificationRestService notificationRestService;
+	private final PolicyRestService policyRestService;
+	private final PolicyStatusRestService policyStatusRestService;
 	private final ProjectRestService projectRestService;
 	private final ProjectVersionRestService projectVersionRestService;
-	private final PolicyStatusRestService policyStatusRestService;
-	private final CodeLocationRestService codeLocationRestService;
 	private final ScanSummaryRestService scanSummaryRestService;
-	private final BomImportRestService bomImportRestService;
+	private final UserRestService userRestService;
+	private final VersionBomPolicyRestService versionBomPolicyRestService;
+	private final VulnerabilityRestService vulnerabilityRestService;
 
 	public DataServicesFactory(final RestConnection restConnection) {
 		this.restConnection = restConnection;
 
+		bomImportRestService = new BomImportRestService(restConnection);
+		codeLocationRestService = new CodeLocationRestService(restConnection, gson, jsonParser);
+		componentVersionRestService = new ComponentVersionRestService(restConnection, gson, jsonParser);
+		notificationRestService = new NotificationRestService(restConnection, gson, jsonParser);
+		policyRestService = new PolicyRestService(restConnection, gson, jsonParser);
+		policyStatusRestService = new PolicyStatusRestService(restConnection, gson, jsonParser);
 		projectRestService = new ProjectRestService(restConnection, gson, jsonParser);
 		projectVersionRestService = new ProjectVersionRestService(restConnection, gson, jsonParser);
-		policyStatusRestService = new PolicyStatusRestService(restConnection, gson, jsonParser);
-		codeLocationRestService = new CodeLocationRestService(restConnection, gson, jsonParser);
 		scanSummaryRestService = new ScanSummaryRestService(restConnection, gson, jsonParser);
-		bomImportRestService = new BomImportRestService(restConnection);
+		userRestService = new UserRestService(restConnection, gson, jsonParser);
+		versionBomPolicyRestService = new VersionBomPolicyRestService(restConnection, gson, jsonParser);
+		vulnerabilityRestService = new VulnerabilityRestService(restConnection, gson, jsonParser);
 	}
 
 	public PolicyStatusDataService createPolicyStatusDataService() {
@@ -69,6 +87,30 @@ public class DataServicesFactory {
 		return jsonParser;
 	}
 
+	public BomImportRestService getBomImportRestService() {
+		return bomImportRestService;
+	}
+
+	public CodeLocationRestService getCodeLocationRestService() {
+		return codeLocationRestService;
+	}
+
+	public ComponentVersionRestService getComponentVersionRestService() {
+		return componentVersionRestService;
+	}
+
+	public NotificationRestService getNotificationRestService() {
+		return notificationRestService;
+	}
+
+	public PolicyRestService getPolicyRestService() {
+		return policyRestService;
+	}
+
+	public PolicyStatusRestService getPolicyStatusRestService() {
+		return policyStatusRestService;
+	}
+
 	public ProjectRestService getProjectRestService() {
 		return projectRestService;
 	}
@@ -77,20 +119,20 @@ public class DataServicesFactory {
 		return projectVersionRestService;
 	}
 
-	public PolicyStatusRestService getPolicyStatusRestService() {
-		return policyStatusRestService;
-	}
-
-	public CodeLocationRestService getCodeLocationRestService() {
-		return codeLocationRestService;
-	}
-
 	public ScanSummaryRestService getScanSummaryRestService() {
 		return scanSummaryRestService;
 	}
 
-	public BomImportRestService getBomImportRestService() {
-		return bomImportRestService;
+	public UserRestService getUserRestService() {
+		return userRestService;
+	}
+
+	public VersionBomPolicyRestService getVersionBomPolicyRestService() {
+		return versionBomPolicyRestService;
+	}
+
+	public VulnerabilityRestService getVulnerabilityRestService() {
+		return vulnerabilityRestService;
 	}
 
 }
