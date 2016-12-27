83daad2174e04f6824239a02da8b6ea263f96676
Eric Kerwin <ekerwin@blackducksoftware.com>
2016-12-26 21:36:29 -0500

    cleaning copyright

--- src/main/resources/riskreport/web/js/HubBomReportFunctions.js
+++ src/main/resources/riskreport/web/js/HubBomReportFunctions.js
@@ -1,7 +1,7 @@
 /*
  * Hub Common
  *
- * Copyright (C) 2016 Black Duck Software, Inc..
+ * Copyright (C) 2016 Black Duck Software, Inc.
  * http://www.blackducksoftware.com/
  *
  * Licensed to the Apache Software Foundation (ASF) under one
