f9a6dc13a7b6b625d072c0b430a98082ca736974
jrichard <jrichard@blackducksoftware.com>
2017-03-10 16:03:37 -0500

    Adding the ability to point to a jssecacerts file as well as a cacerts file

--- src/main/java/com/blackducksoftware/integration/hub/cli/CLIDownloadService.java
+++ src/main/java/com/blackducksoftware/integration/hub/cli/CLIDownloadService.java
@@ -190,7 +190,6 @@ public class CLIDownloadService {
 
     private void updateJreSecurity(final IntLogger logger, final CLILocation cliLocation, final CIEnvironmentVariables ciEnvironmentVariables)
             throws IOException {
-        final String cacertsFilename = "cacerts";
         if (ciEnvironmentVariables.containsKey(CIEnvironmentVariables.BDS_CACERTS_OVERRIDE)) {
             logger.trace("Found the variable : " + CIEnvironmentVariables.BDS_CACERTS_OVERRIDE + ", using value : " + ciEnvironmentVariables
                     .getValue(CIEnvironmentVariables.BDS_CACERTS_OVERRIDE));
@@ -204,7 +203,12 @@ public class CLIDownloadService {
                     .getValue(CIEnvironmentVariables.BDS_CACERTS_OVERRIDE);
             final File customCacerts = new File(customCacertsPath);
 
-            final File cacerts = new File(securityDirectory, cacertsFilename);
+            String cacertsFilename = "cacerts";
+            File cacerts = new File(securityDirectory, cacertsFilename);
+            if (!cacerts.exists()) {
+                cacertsFilename = "jssecacerts";
+                cacerts = new File(securityDirectory, cacertsFilename);
+            }
             final File cacertsBackup = new File(securityDirectory, cacertsFilename + System.currentTimeMillis());
 
             try {
