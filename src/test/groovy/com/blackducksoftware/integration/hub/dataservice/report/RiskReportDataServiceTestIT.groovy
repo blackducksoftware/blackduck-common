d7302a4694e73e4a7d81ee9d5db252c4b3124349
jrichard <jrichard@blackducksoftware.com>
2017-07-21 14:55:25 -0400

    Moving report test to IT test

--- src/test/groovy/com/blackducksoftware/integration/hub/dataservice/report/RiskReportDataServiceTestIT.groovy
+++ src/test/groovy/com/blackducksoftware/integration/hub/dataservice/report/RiskReportDataServiceTestIT.groovy
@@ -12,17 +12,34 @@
 package com.blackducksoftware.integration.hub.dataservice.report
 
 import org.junit.Assert
+import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.TemporaryFolder
 
 import com.blackducksoftware.integration.hub.rest.RestConnectionTestHelper
 import com.blackducksoftware.integration.hub.service.HubServicesFactory
 import com.blackducksoftware.integration.log.IntLogger
 
-class RiskReportDataServiceTest {
-    //    @Rule
-    //    public TemporaryFolder folderForReport = new TemporaryFolder()
+class RiskReportDataServiceTestIT {
+    @Rule
+    public TemporaryFolder folderForReport = new TemporaryFolder()
 
     private final RestConnectionTestHelper restConnectionTestHelper = new RestConnectionTestHelper()
+
+    @Test
+    public void createReportPdfFileTest(){
+        final String testProjectName = restConnectionTestHelper.getProperty("TEST_PROJECT")
+        final String testProjectVersionName = restConnectionTestHelper.getProperty("TEST_VERSION")
+
+        final HubServicesFactory hubServicesFactory = restConnectionTestHelper.createHubServicesFactory()
+        final IntLogger logger = hubServicesFactory.getRestConnection().logger
+        RiskReportDataService riskReportDataService = hubServicesFactory.createRiskReportDataService(logger, 30000)
+        //File folderForReport = folderForReport.getRoot()
+        File folderForReport = new File('.')
+        File pdfFile = riskReportDataService.createReportPdfFile(folderForReport, testProjectName, testProjectVersionName)
+        Assert.assertNotNull(pdfFile)
+        Assert.assertTrue(pdfFile.exists())
+    }
 
     @Test
     public void createReportFilesTest(){
@@ -32,9 +49,18 @@ class RiskReportDataServiceTest {
         final HubServicesFactory hubServicesFactory = restConnectionTestHelper.createHubServicesFactory()
         final IntLogger logger = hubServicesFactory.getRestConnection().logger
         RiskReportDataService riskReportDataService = hubServicesFactory.createRiskReportDataService(logger, 30000)
-        File folderForReport = new File('.')
-        File pdfFile = riskReportDataService.createReportPdfFile(folderForReport, testProjectName, testProjectVersionName)
-        Assert.assertNotNull(pdfFile)
-        Assert.assertTrue(pdfFile.exists())
+        File folderForReport = folderForReport.getRoot()
+        riskReportDataService.createReportFiles(folderForReport, testProjectName, testProjectVersionName)
+
+        File[] reportFiles = folderForReport.listFiles();
+        Assert.assertNotNull(reportFiles)
+        Assert.assertTrue(reportFiles.size() > 0)
+        Map<String, File> reportFileMap = reportFiles.collectEntries{
+            [it.getName(), it]
+        }
+        Assert.assertNotNull(reportFileMap.get('js'))
+        Assert.assertNotNull(reportFileMap.get('css'))
+        Assert.assertNotNull(reportFileMap.get('images'))
+        Assert.assertNotNull(reportFileMap.get('riskreport.html'))
     }
 }
