d78cb1384cd9b9617431de2958fc392b6e954803
Patrick Conway <pconway@pcm-pconway-1.local>
2017-07-27 15:55:55 -0400

    checking for bomcomponent validity in reporting

--- src/test/groovy/com/blackducksoftware/integration/hub/dataservice/report/RiskReportDataServiceTestIT.groovy
+++ src/test/groovy/com/blackducksoftware/integration/hub/dataservice/report/RiskReportDataServiceTestIT.groovy
@@ -1,15 +1,29 @@
-/*
- * Copyright (C) 2017 Black Duck Software Inc.
- * http://www.blackducksoftware.com/
- * All rights reserved.
+/**
+ * Hub Common
  *
- * This software is the confidential and proprietary information of
- * Black Duck Software ("Confidential Information"). You shall not
- * disclose such Confidential Information and shall use it only in
- * accordance with the terms of the license agreement you entered into
- * with Black Duck Software.
+ * Copyright (C) 2017 Black Duck Software, Inc.
+ * http://www.blackducksoftware.com/
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 package com.blackducksoftware.integration.hub.dataservice.report
+
+import java.util.zip.Adler32
 
 import org.junit.Assert
 import org.junit.Rule
@@ -28,8 +42,10 @@ class RiskReportDataServiceTestIT {
 
     @Test
     public void createReportPdfFileTest(){
-        final String testProjectName = restConnectionTestHelper.getProperty("TEST_PROJECT")
-        final String testProjectVersionName = restConnectionTestHelper.getProperty("TEST_VERSION")
+//        final String testProjectName = restConnectionTestHelper.getProperty("TEST_PROJECT")
+//        final String testProjectVersionName = restConnectionTestHelper.getProperty("TEST_VERSION")
+		final String testProjectName = "ReportingProject"
+		final String testProjectVersionName = "1"
 
         final HubServicesFactory hubServicesFactory = restConnectionTestHelper.createHubServicesFactory()
         final IntLogger logger = hubServicesFactory.getRestConnection().logger
@@ -41,26 +57,26 @@ class RiskReportDataServiceTestIT {
         Assert.assertTrue(pdfFile.exists())
     }
 
-    @Test
-    public void createReportFilesTest(){
-        final String testProjectName = restConnectionTestHelper.getProperty("TEST_PROJECT")
-        final String testProjectVersionName = restConnectionTestHelper.getProperty("TEST_VERSION")
-
-        final HubServicesFactory hubServicesFactory = restConnectionTestHelper.createHubServicesFactory()
-        final IntLogger logger = hubServicesFactory.getRestConnection().logger
-        RiskReportDataService riskReportDataService = hubServicesFactory.createRiskReportDataService(logger, 30000)
-        File folderForReport = folderForReport.getRoot()
-        riskReportDataService.createReportFiles(folderForReport, testProjectName, testProjectVersionName)
-
-        File[] reportFiles = folderForReport.listFiles();
-        Assert.assertNotNull(reportFiles)
-        Assert.assertTrue(reportFiles.size() > 0)
-        Map<String, File> reportFileMap = reportFiles.collectEntries{
-            [it.getName(), it]
-        }
-        Assert.assertNotNull(reportFileMap.get('js'))
-        Assert.assertNotNull(reportFileMap.get('css'))
-        Assert.assertNotNull(reportFileMap.get('images'))
-        Assert.assertNotNull(reportFileMap.get('riskreport.html'))
-    }
+//    @Test
+//    public void createReportFilesTest(){
+//        final String testProjectName = restConnectionTestHelper.getProperty("TEST_PROJECT")
+//        final String testProjectVersionName = restConnectionTestHelper.getProperty("TEST_VERSION")
+//
+//        final HubServicesFactory hubServicesFactory = restConnectionTestHelper.createHubServicesFactory()
+//        final IntLogger logger = hubServicesFactory.getRestConnection().logger
+//        RiskReportDataService riskReportDataService = hubServicesFactory.createRiskReportDataService(logger, 30000)
+//        File folderForReport = folderForReport.getRoot()
+//        riskReportDataService.createReportFiles(folderForReport, testProjectName, testProjectVersionName)
+//
+//        File[] reportFiles = folderForReport.listFiles();
+//        Assert.assertNotNull(reportFiles)
+//        Assert.assertTrue(reportFiles.size() > 0)
+//        Map<String, File> reportFileMap = reportFiles.collectEntries{
+//            [it.getName(), it]
+//        }
+//        Assert.assertNotNull(reportFileMap.get('js'))
+//        Assert.assertNotNull(reportFileMap.get('css'))
+//        Assert.assertNotNull(reportFileMap.get('images'))
+//        Assert.assertNotNull(reportFileMap.get('riskreport.html'))
+//    }
 }
