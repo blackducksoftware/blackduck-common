ad5c8788b68dbc38aaa11858e3c063c177a98d74
Paulo Santos <psantos@blackducksoftware.com>
2017-11-03 16:01:42 -0400

    Add a media types constant class and update the vulnerability service

---
+++ src/main/groovy/com/blackducksoftware/integration/hub/api/HubMediaTypes.java
@@ -0,0 +1,5 @@
+package com.blackducksoftware.integration.hub.api;
+
+public class HubMediaTypes {
+    public final static String VULNERABILITY_REQUEST_SERVICE = "application/vnd.blackducksoftware.list-1+json";
+}
