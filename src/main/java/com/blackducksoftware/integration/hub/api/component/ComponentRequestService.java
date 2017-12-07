f3953157e5e7166907a4bf0465bdbb2c234ec91f
Richard Otte <rotte@blackducksoftware.com>
2017-12-07 13:12:22 -0500

    Component services now take an externalId instead of a namespaced gav

--- src/main/java/com/blackducksoftware/integration/hub/api/component/ComponentRequestService.java
+++ src/main/java/com/blackducksoftware/integration/hub/api/component/ComponentRequestService.java
@@ -29,7 +29,10 @@ import static com.blackducksoftware.integration.hub.api.UrlConstants.SEGMENT_COM
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
+
 import com.blackducksoftware.integration.exception.IntegrationException;
+import com.blackducksoftware.integration.hub.bdio.model.externalid.ExternalId;
 import com.blackducksoftware.integration.hub.exception.HubIntegrationException;
 import com.blackducksoftware.integration.hub.model.response.ComponentSearchResultResponse;
 import com.blackducksoftware.integration.hub.request.HubPagedRequest;
@@ -43,20 +46,20 @@ public class ComponentRequestService extends HubResponseService {
         super(restConnection);
     }
 
-    public List<ComponentSearchResultResponse> getAllComponents(final String namespace, final String groupId, final String artifactId, final String version) throws IntegrationException {
-        final String componentQuery = String.format("id:%s|%s|%s|%s", namespace, groupId, artifactId, version);
+    public List<ComponentSearchResultResponse> getAllComponents(final ExternalId externalId) throws IntegrationException {
+        final String componentQuery = String.format("id:%s", StringUtils.join(externalId.getExternalIdPieces(), "|"));
         final HubPagedRequest hubPagedRequest = getHubRequestFactory().createPagedRequest(COMPONENT_SEGMENTS, componentQuery);
 
         final List<ComponentSearchResultResponse> allComponents = getAllItems(hubPagedRequest, ComponentSearchResultResponse.class);
         return allComponents;
     }
 
-    public ComponentSearchResultResponse getExactComponentMatch(final String namespace, final String groupId, final String artifactId, final String version) throws IntegrationException {
-        final List<ComponentSearchResultResponse> allComponents = getAllComponents(namespace, groupId, artifactId, version);
+    public ComponentSearchResultResponse getExactComponentMatch(final ExternalId externalId) throws IntegrationException {
+        final List<ComponentSearchResultResponse> allComponents = getAllComponents(externalId);
         for (final ComponentSearchResultResponse componentItem : allComponents) {
             if (componentItem.originId != null) {
-                final String exactMatch = String.format("%s:%s:%s", groupId, artifactId, version);
-                if (componentItem.originId.equals(exactMatch)) {
+                final String matchId = externalId.createHubOriginId();
+                if (componentItem.originId.equals(matchId)) {
                     return componentItem;
                 }
             }
