1d6819c6b13cd70c741e823bc9e87b307db537ae
Eric Kerwin <ekerwin@kingofnerds.com>
2018-04-14 19:24:33 -0400

    adding utility method for getting responses

--- src/main/java/com/blackducksoftware/integration/hub/service/HubService.java
+++ src/main/java/com/blackducksoftware/integration/hub/service/HubService.java
@@ -223,7 +223,10 @@ public class HubService {
 
     public <T extends HubResponse> List<T> getResponses(final String uri, final Class<T> responseClass, final boolean getAll) throws IntegrationException {
         final Request.Builder requestBuilder = RequestFactory.createCommonGetRequestBuilder(uri);
-        requestBuilder.uri(uri);
+        return getResponses(responseClass, requestBuilder, getAll);
+    }
+
+    public <T extends HubResponse> List<T> getResponses(final Class<T> responseClass, final Request.Builder requestBuilder, final boolean getAll) throws IntegrationException {
         return hubResponsesTransformer.getResponses(new PagedRequest(requestBuilder), responseClass, getAll, null);
     }
 
