bd6545a72d97820188884eff66680776f18fb854
Eric Kerwin <ekerwin@kingofnerds.com>
2018-05-16 20:28:44 -0400

    fixing test

--- src/test/groovy/com/blackducksoftware/integration/hub/configuration/HubServerConfigBuilderTest.groovy
+++ src/test/groovy/com/blackducksoftware/integration/hub/configuration/HubServerConfigBuilderTest.groovy
@@ -9,15 +9,15 @@ class HubServerConfigBuilderTest {
         def properties = [BLACKDUCK_HUB_URL: 'test url', BLACKDUCK_HUB_USERNAME: 'user', BLACKDUCK_HUB_PASSWORD: 'password']
 
         def hubServerConfigBuilder = new HubServerConfigBuilder()
-        Assert.assertNull(hubServerConfigBuilder.values.get(HubServerConfigProperty.URL))
-        Assert.assertNull(hubServerConfigBuilder.values.get(HubServerConfigProperty.USERNAME))
-        Assert.assertNull(hubServerConfigBuilder.values.get(HubServerConfigProperty.PASSWORD))
+        Assert.assertNull(hubServerConfigBuilder.values.get(HubServerConfigBuilder.Property.URL))
+        Assert.assertNull(hubServerConfigBuilder.values.get(HubServerConfigBuilder.Property.USERNAME))
+        Assert.assertNull(hubServerConfigBuilder.values.get(HubServerConfigBuilder.Property.PASSWORD))
 
         hubServerConfigBuilder.setFromProperties(properties)
 
-        Assert.assertEquals('test url', hubServerConfigBuilder.values.get(HubServerConfigProperty.URL))
-        Assert.assertEquals('user', hubServerConfigBuilder.values.get(HubServerConfigProperty.USERNAME))
-        Assert.assertEquals('password', hubServerConfigBuilder.values.get(HubServerConfigProperty.PASSWORD))
+        Assert.assertEquals('test url', hubServerConfigBuilder.values.get(HubServerConfigBuilder.Property.URL))
+        Assert.assertEquals('user', hubServerConfigBuilder.values.get(HubServerConfigBuilder.Property.USERNAME))
+        Assert.assertEquals('password', hubServerConfigBuilder.values.get(HubServerConfigBuilder.Property.PASSWORD))
     }
 
     @Test
@@ -25,14 +25,14 @@ class HubServerConfigBuilderTest {
         def properties = [BLACKDUCK_HUB_URL: 'test url', "blackduck.hub.username": 'user', BLACKDUCK_HUB_PASSWORD: 'password']
 
         def hubServerConfigBuilder = new HubServerConfigBuilder()
-        Assert.assertNull(hubServerConfigBuilder.values.get(HubServerConfigProperty.URL))
-        Assert.assertNull(hubServerConfigBuilder.values.get(HubServerConfigProperty.USERNAME))
-        Assert.assertNull(hubServerConfigBuilder.values.get(HubServerConfigProperty.PASSWORD))
+        Assert.assertNull(hubServerConfigBuilder.values.get(HubServerConfigBuilder.Property.URL))
+        Assert.assertNull(hubServerConfigBuilder.values.get(HubServerConfigBuilder.Property.USERNAME))
+        Assert.assertNull(hubServerConfigBuilder.values.get(HubServerConfigBuilder.Property.PASSWORD))
 
         hubServerConfigBuilder.setFromProperties(properties)
 
-        Assert.assertEquals('test url', hubServerConfigBuilder.values.get(HubServerConfigProperty.URL))
-        Assert.assertEquals('user', hubServerConfigBuilder.values.get(HubServerConfigProperty.USERNAME))
-        Assert.assertEquals('password', hubServerConfigBuilder.values.get(HubServerConfigProperty.PASSWORD))
+        Assert.assertEquals('test url', hubServerConfigBuilder.values.get(HubServerConfigBuilder.Property.URL))
+        Assert.assertEquals('user', hubServerConfigBuilder.values.get(HubServerConfigBuilder.Property.USERNAME))
+        Assert.assertEquals('password', hubServerConfigBuilder.values.get(HubServerConfigBuilder.Property.PASSWORD))
     }
 }
