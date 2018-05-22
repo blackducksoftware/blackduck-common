8e45937c77021cafb3a300a41d236d1df3a87964
Eric Kerwin <ekerwin@kingofnerds.com>
2018-05-21 20:38:18 -0400

    removing the simple prefix

--- src/main/java/com/blackducksoftware/integration/hub/notification/NotificationDetailResult.java
+++ src/main/java/com/blackducksoftware/integration/hub/notification/NotificationDetailResult.java
@@ -1,3 +1,26 @@
+/**
+ * hub-common
+ *
+ * Copyright (C) 2018 Black Duck Software, Inc.
+ * http://www.blackducksoftware.com/
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package com.blackducksoftware.integration.hub.notification;
 
 import java.util.Date;
@@ -18,10 +41,10 @@ public class NotificationDetailResult {
     private final String contentDetailKey;
     private final Optional<NotificationStateRequestStateType> notificationState;
 
-    private final List<NotificationContentDetail> simpleNotificationContentDetails;
+    private final List<NotificationContentDetail> notificationContentDetails;
 
     public NotificationDetailResult(final NotificationContent notificationContent, final String contentType, final Date createdAt, final NotificationType type, final String notificationGroup, final String contentDetailKey,
-            final Optional<NotificationStateRequestStateType> notificationState, final List<NotificationContentDetail> simpleNotificationContentDetails) {
+            final Optional<NotificationStateRequestStateType> notificationState, final List<NotificationContentDetail> notificationContentDetails) {
         this.notificationContent = notificationContent;
         this.contentType = contentType;
         this.createdAt = createdAt;
@@ -29,7 +52,7 @@ public class NotificationDetailResult {
         this.notificationGroup = notificationGroup;
         this.contentDetailKey = contentDetailKey;
         this.notificationState = notificationState;
-        this.simpleNotificationContentDetails = simpleNotificationContentDetails;
+        this.notificationContentDetails = notificationContentDetails;
     }
 
     public NotificationContent getNotificationContent() {
@@ -60,8 +83,8 @@ public class NotificationDetailResult {
         return notificationState;
     }
 
-    public List<NotificationContentDetail> getSimpleNotificationContentDetails() {
-        return simpleNotificationContentDetails;
+    public List<NotificationContentDetail> getNotificationContentDetails() {
+        return notificationContentDetails;
     }
 
 }
