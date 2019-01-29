03816fb2d86985d1c18cdbff931492209ea2f163
ekerwin <ekerwin@kingofnerds.com>
2019-01-29 12:43:08 -0500

    creating initial classes to support waiting for binary scan

---
+++ src/main/java/com/synopsys/integration/blackduck/codelocation/binaryscanner/BinaryScanOutput.java
@@ -0,0 +1,6 @@
+package com.synopsys.integration.blackduck.codelocation.binaryscanner;
+
+import com.synopsys.integration.blackduck.codelocation.CodeLocationBatchOutput;
+
+public class BinaryScanOutput extends CodeLocationBatchOutput {
+}
