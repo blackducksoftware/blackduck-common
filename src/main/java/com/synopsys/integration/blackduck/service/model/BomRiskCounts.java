d5ad53d1932e32d98e0270d40bf3606be88f750c
Eric Kerwin <ekerwin@synopsys.com>
2020-03-25 16:14:12 -0400

    decreasing complexity

--- src/main/java/com/synopsys/integration/blackduck/service/model/BomRiskCounts.java
+++ src/main/java/com/synopsys/integration/blackduck/service/model/BomRiskCounts.java
@@ -23,69 +23,46 @@
 package com.synopsys.integration.blackduck.service.model;
 
 import com.synopsys.integration.blackduck.api.generated.component.ComponentVersionRiskProfileRiskDataCountsView;
-import com.synopsys.integration.blackduck.api.generated.enumeration.ComponentVersionRiskProfileRiskDataCountsCountTypeType;
 
 import java.math.BigDecimal;
-import java.util.function.Consumer;
 
 import static com.synopsys.integration.blackduck.api.generated.enumeration.ComponentVersionRiskProfileRiskDataCountsCountTypeType.*;
 
 public class BomRiskCounts {
-    private BigDecimalWrapper high = new BigDecimalWrapper();
-    private BigDecimalWrapper medium = new BigDecimalWrapper();
-    private BigDecimalWrapper low = new BigDecimalWrapper();
+    private BigDecimal high = BigDecimal.ZERO;
+    private BigDecimal medium = BigDecimal.ZERO;
+    private BigDecimal low = BigDecimal.ZERO;
 
     public void add(ComponentVersionRiskProfileRiskDataCountsView countsView) {
-        ComponentVersionRiskProfileRiskDataCountsCountTypeType countType = countsView.getCountType();
-        if (HIGH == countType) {
-            addTo(high::add, countsView);
-        } else if (MEDIUM == countType) {
-            addTo(medium::add, countsView);
-        } else if (LOW == countType) {
-            addTo(low::add, countsView);
+        if (HIGH == countsView.getCountType()) {
+            high = high.add(countsView.getCount());
+        } else if (MEDIUM == countsView.getCountType()) {
+            medium = medium.add(countsView.getCount());
+        } else if (LOW == countsView.getCountType()) {
+            low = low.add(countsView.getCount());
         }
     }
 
     public void add(BomRiskCounts bomRiskCounts) {
-        if (bomRiskCounts.high.isGreaterThanZero()) {
-            addTo(high::add);
-        } else if (bomRiskCounts.medium.isGreaterThanZero()) {
-            addTo(medium::add);
-        } else if (bomRiskCounts.low.isGreaterThanZero()) {
-            addTo(low::add);
+        if (bomRiskCounts.getHigh() > 0) {
+            high = high.add(BigDecimal.ONE);
+        } else if (bomRiskCounts.getMedium() > 0) {
+            medium = medium.add(BigDecimal.ONE);
+        } else if (bomRiskCounts.getLow() > 0) {
+            low = low.add(BigDecimal.ONE);
         }
     }
 
     public int getHigh() {
-        return high.bigDecimal.intValue();
+        return high.intValue();
     }
 
     public int getMedium() {
-        return medium.bigDecimal.intValue();
+        return medium.intValue();
     }
 
     public int getLow() {
-        return low.bigDecimal.intValue();
-    }
-
-    private void addTo(Consumer<BigDecimal> adder, ComponentVersionRiskProfileRiskDataCountsView countsView) {
-        adder.accept(countsView.getCount());
-    }
-
-    private void addTo(Consumer<BigDecimal> adder) {
-        adder.accept(BigDecimal.ONE);
-    }
-
-    private class BigDecimalWrapper {
-        public BigDecimal bigDecimal = BigDecimal.ZERO;
-
-        public void add(BigDecimal toAdd) {
-            this.bigDecimal = this.bigDecimal.add(toAdd);
-        }
-
-        public boolean isGreaterThanZero() {
-            return bigDecimal.compareTo(BigDecimal.ZERO) > 0;
-        }
+        return low.intValue();
     }
 
 }
