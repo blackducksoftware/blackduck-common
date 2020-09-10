73e849eda5616e6baafa291c8c9d4a3df1c24385
Dana Maxfield <danam@synopsys.com>
2020-09-10 10:08:14 -0400

    Update from changes to RequestFactory

--- src/test/java/com/synopsys/integration/blackduck/http/client/BlackDuckHttpClientTestIT.java
+++ src/test/java/com/synopsys/integration/blackduck/http/client/BlackDuckHttpClientTestIT.java
@@ -20,7 +20,6 @@ import com.synopsys.integration.blackduck.api.core.BlackDuckPath;
 import com.synopsys.integration.blackduck.api.core.BlackDuckView;
 import com.synopsys.integration.blackduck.api.core.response.BlackDuckPathMultipleResponses;
 import com.synopsys.integration.blackduck.api.generated.discovery.ApiDiscovery;
-import com.synopsys.integration.blackduck.api.generated.discovery.BlackDuckMediaTypeDiscovery;
 import com.synopsys.integration.blackduck.api.generated.view.UserView;
 import com.synopsys.integration.blackduck.configuration.BlackDuckServerConfig;
 import com.synopsys.integration.blackduck.configuration.BlackDuckServerConfigBuilder;
@@ -48,7 +47,7 @@ public class BlackDuckHttpClientTestIT {
     private static final BlackDuckPath API_TOKEN_LINK = new BlackDuckPath("/api/current-user/tokens");
     private static final BlackDuckPathMultipleResponses<ApiTokenView> API_TOKEN_LINK_RESPONSE = new BlackDuckPathMultipleResponses<>(BlackDuckHttpClientTestIT.API_TOKEN_LINK, ApiTokenView.class);
 
-    private RequestFactory requestFactory = new RequestFactory(new BlackDuckMediaTypeDiscovery());
+    private RequestFactory requestFactory = new RequestFactory();
     private HttpUrl blackDuckUrl = INT_HTTP_CLIENT_TEST_HELPER.getIntegrationBlackDuckServerUrl();
     private String username = INT_HTTP_CLIENT_TEST_HELPER.getTestUsername();
     private String password = INT_HTTP_CLIENT_TEST_HELPER.getTestPassword();
