6bf68eb317b70c6902fb993107ef59568ee362f9
Dana Maxfield <danam@synopsys.com>
2020-09-13 11:10:52 -0400

    Reformating for test package http/client

--- src/test/java/com/synopsys/integration/blackduck/http/client/MockWebServerUtil.java
+++ src/test/java/com/synopsys/integration/blackduck/http/client/MockWebServerUtil.java
@@ -1,10 +1,11 @@
 package com.synopsys.integration.blackduck.http.client;
 
+import org.apache.http.HttpHeaders;
+
 import okhttp3.mockwebserver.Dispatcher;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 import okhttp3.mockwebserver.RecordedRequest;
-import org.apache.http.HttpHeaders;
 
 public class MockWebServerUtil {
     public static void setupRedirecting(MockWebServer server, String destinationUrl) {
@@ -12,8 +13,8 @@ public class MockWebServerUtil {
             @Override
             public MockResponse dispatch(RecordedRequest request) throws InterruptedException {
                 return new MockResponse()
-                        .setResponseCode(308)
-                        .addHeader(HttpHeaders.LOCATION, destinationUrl + request.getPath());
+                           .setResponseCode(308)
+                           .addHeader(HttpHeaders.LOCATION, destinationUrl + request.getPath());
             }
         };
         server.setDispatcher(dispatcher);
