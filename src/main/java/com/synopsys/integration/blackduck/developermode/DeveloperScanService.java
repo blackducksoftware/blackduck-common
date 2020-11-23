08e2a23e500e2a19e6fb33a570e0c2fe970ee92b
psantos1113 <psantos@synopsys.com>
2020-11-23 10:24:08 -0500

    feat: Add ability to expand a bdio zip file.

--- src/main/java/com/synopsys/integration/blackduck/developermode/DeveloperScanService.java
+++ src/main/java/com/synopsys/integration/blackduck/developermode/DeveloperScanService.java
@@ -22,13 +22,19 @@
  */
 package com.synopsys.integration.blackduck.developermode;
 
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 import org.apache.commons.io.FileUtils;
 
@@ -64,6 +70,15 @@ public class DeveloperScanService {
             throw new IllegalArgumentException(String.format("Unknown file extension. Cannot perform developer scan. Path: %s", bdioFile.getAbsolutePath()));
         }
         List<BdioContent> bdioContentList = new ArrayList<>();
+        try (ZipFile zipFile = new ZipFile(bdioFile)) {
+            Enumeration<? extends ZipEntry> entries = zipFile.entries();
+            while (entries.hasMoreElements()) {
+                ZipEntry entry = entries.nextElement();
+                bdioContentList.add(readEntryContent(zipFile, entry));
+            }
+        } catch (IOException ex) {
+            throw new IntegrationException(String.format("Exception unzipping BDIO file. Path: %s", bdioFile.getAbsolutePath()), ex);
+        }
         return uploadFilesAndWait(scanType, bdioContentList);
     }
 
@@ -82,6 +97,24 @@ public class DeveloperScanService {
             throw new IntegrationException("Couldn't read BDIO file", ex);
         }
         return uploadFilesAndWait(scanType, bdioContentList);
+    }
+
+    private BdioContent readEntryContent(ZipFile zipFile, ZipEntry entry) throws IntegrationException {
+        String entryContent;
+        byte[] buffer = new byte[1024];
+        try (InputStream zipInputStream = zipFile.getInputStream(entry);
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(byteArrayOutputStream)) {
+            int length;
+            while ((length = zipInputStream.read(buffer)) > 0) {
+                bufferedOutputStream.write(buffer, 0, length);
+            }
+            bufferedOutputStream.flush();
+            entryContent = new String(byteArrayOutputStream.toByteArray(), StandardCharsets.UTF_8);
+        } catch (IOException ex) {
+            throw new IntegrationException(String.format("Error reading entry %s", entry.getName()), ex);
+        }
+        return new BdioContent(entry.getName(), entryContent);
     }
 
     private BomMatchDeveloperView uploadFilesAndWait(String scanType, List<BdioContent> bdioFiles) throws IntegrationException {
