69164d2de89466fd6e0ffde8c0abce3b76efb8d3
psantos1113 <psantos@synopsys.com>
2020-11-25 08:55:06 -0500

    test: Update the tests to pass.

--- src/test/java/com/synopsys/integration/blackduck/developermode/DeveloperScanServiceTestIT.java
+++ src/test/java/com/synopsys/integration/blackduck/developermode/DeveloperScanServiceTestIT.java
@@ -1,5 +1,6 @@
 package com.synopsys.integration.blackduck.developermode;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.fail;
 
@@ -26,7 +27,7 @@ public class DeveloperScanServiceTestIT {
             DeveloperScanService developerScanService = blackDuckServicesFactory.createDeveloperScanService();
             File bdioFile = new File(getClass().getResource("/bdio/developer_scan/").getFile());
             int timeout = intHttpClientTestHelper.getBlackDuckServerConfig().getTimeout();
-            developerScanService.performDeveloperScan("blackduck-common test", bdioFile, timeout);
+            developerScanService.performDeveloperScan(bdioFile, timeout);
             fail();
         } catch (IllegalArgumentException ex) {
             // pass
@@ -41,7 +42,7 @@ public class DeveloperScanServiceTestIT {
             DeveloperScanService developerScanService = blackDuckServicesFactory.createDeveloperScanService();
             File bdioFile = new File("/bdio/developer_scan/badPath.bdio");
             int timeout = intHttpClientTestHelper.getBlackDuckServerConfig().getTimeout();
-            developerScanService.performDeveloperScan("blackduck-common test", bdioFile, timeout);
+            developerScanService.performDeveloperScan(bdioFile, timeout);
             fail();
         } catch (IllegalArgumentException ex) {
             // pass
@@ -57,7 +58,7 @@ public class DeveloperScanServiceTestIT {
             BlackDuckServicesFactory blackDuckServicesFactory = intHttpClientTestHelper.createBlackDuckServicesFactory();
             DeveloperScanService developerScanService = blackDuckServicesFactory.createDeveloperScanService();
             int timeout = intHttpClientTestHelper.getBlackDuckServerConfig().getTimeout();
-            developerScanService.performDeveloperScan("blackduck-common test", bdioFile, timeout);
+            developerScanService.performDeveloperScan(bdioFile, timeout);
             fail();
         } catch (IllegalArgumentException ex) {
             // pass
@@ -72,7 +73,7 @@ public class DeveloperScanServiceTestIT {
             DeveloperScanService developerScanService = blackDuckServicesFactory.createDeveloperScanService();
             File bdioFile = new File(getClass().getResource("/bdio/developer_scan/developerScanMissingHeader.bdio").getFile());
             int timeout = intHttpClientTestHelper.getBlackDuckServerConfig().getTimeout();
-            developerScanService.performDeveloperScan("blackduck-common developer mode service test", bdioFile, timeout);
+            developerScanService.performDeveloperScan(bdioFile, timeout);
             fail();
         } catch (BlackDuckIntegrationException ex) {
             // pass
@@ -86,7 +87,8 @@ public class DeveloperScanServiceTestIT {
         DeveloperScanService developerScanService = blackDuckServicesFactory.createDeveloperScanService();
         File bdioFile = new File(getClass().getResource("/bdio/developer_scan/developerScanTest.bdio").getFile());
         int timeout = intHttpClientTestHelper.getBlackDuckServerConfig().getTimeout();
-        List<BomMatchDeveloperView> results = developerScanService.performDeveloperScan("blackduck-common developer mode service test", bdioFile, timeout);
+        List<BomMatchDeveloperView> results = developerScanService.performDeveloperScan(bdioFile, timeout);
         assertNotNull(results);
+        assertFalse(results.isEmpty());
     }
 }
