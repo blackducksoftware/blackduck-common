d8f85220f30c2d27d7ad771a92670b748c2c6d2f
Eric Kerwin <ekerwin@synopsys.com>
2020-12-09 14:32:33 -0500

    creating custom ranking of severities

---
+++ src/main/java/com/synopsys/integration/blackduck/api/enumeration/RankedSeverityType.java
@@ -0,0 +1,29 @@
+package com.synopsys.integration.blackduck.api.enumeration;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.synopsys.integration.blackduck.api.generated.enumeration.PolicyRuleSeverityType;
+
+public enum RankedSeverityType {
+    UNSPECIFIED(PolicyRuleSeverityType.UNSPECIFIED),
+    TRIVIAL(PolicyRuleSeverityType.TRIVIAL),
+    MINOR(PolicyRuleSeverityType.MINOR),
+    MAJOR(PolicyRuleSeverityType.MAJOR),
+    CRITICAL(PolicyRuleSeverityType.CRITICAL),
+    BLOCKER(PolicyRuleSeverityType.BLOCKER);
+
+    private final PolicyRuleSeverityType policyRuleSeverityType;
+
+    private RankedSeverityType(PolicyRuleSeverityType unrankedSeverityType) {
+        this.policyRuleSeverityType = unrankedSeverityType;
+    }
+
+    public static List<PolicyRuleSeverityType> getRankedValues() {
+        return Arrays
+                   .stream(values())
+                   .map(t -> t.policyRuleSeverityType)
+                   .collect(Collectors.toList());
+    }
+}
