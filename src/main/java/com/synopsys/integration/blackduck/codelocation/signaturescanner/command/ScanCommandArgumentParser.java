421f689fb8bfd53d31557c28662695f4bb50e094
crowleySynopsys <alex.crowley@synopsys.com>
2021-01-04 12:37:29 -0500

    Updated ScanCommandArgumentParser to account for escaped quotes, updated test.

--- src/main/java/com/synopsys/integration/blackduck/codelocation/signaturescanner/command/ScanCommandArgumentParser.java
+++ src/main/java/com/synopsys/integration/blackduck/codelocation/signaturescanner/command/ScanCommandArgumentParser.java
@@ -1,9 +1,32 @@
+/**
+ * blackduck-common
+ *
+ * Copyright (c) 2021 Synopsys, Inc.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package com.synopsys.integration.blackduck.codelocation.signaturescanner.command;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 
 public class ScanCommandArgumentParser {
@@ -28,7 +51,7 @@ public class ScanCommandArgumentParser {
                     currentArgument.append(" ");
                 }
                 currentArgument.append(text);
-                if (text.endsWith("\"")) {
+                if (text.endsWith("\"") && !text.endsWith("\\\"")) {
                     inQuotes = false;
                     parsedArguments.add(currentArgument.toString());
                 }
@@ -43,11 +66,20 @@ public class ScanCommandArgumentParser {
         if (StringUtils.isBlank(command)) {
             return false;
         }
-        return hasEvenNumberOfQuotes(command);
+        return hasEvenNumberOfNonEscapedQuotes(command);
     }
 
-    private boolean hasEvenNumberOfQuotes(String commandLine) {
-        int pairsOfDoubleQuotes = StringUtils.countMatches(commandLine, "\"") % 2;
-        return pairsOfDoubleQuotes == 0;
+    private boolean hasEvenNumberOfNonEscapedQuotes(String commandLine) {
+        int numberOfNonEscapedQuotes = 0;
+        char quote = '"';
+        char backSlash = '\\';
+        char last = 0;
+        for (char current : commandLine.toCharArray()) {
+            if (current == quote && last != backSlash) {
+                numberOfNonEscapedQuotes++;
+            }
+            last = current;
+        }
+        return (numberOfNonEscapedQuotes % 2) == 0;
     }
 }
--- src/test/java/com/synopsys/integration/blackduck/codelocation/signaturescanner/command/ScanCommandArgumentParserTest.java
+++ src/test/java/com/synopsys/integration/blackduck/codelocation/signaturescanner/command/ScanCommandArgumentParserTest.java
@@ -4,16 +4,54 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ScanCommandArgumentParserTest {
 
     @Test
     public void testParsesArgumentsWithSpaces() {
-        String command = "--upload-source --exclude \"/Users/joe/test folder/\"";
+        String argumentsWithSpaces = "--upload-source --exclude \"/Users/joe/test folder/\"";
         ScanCommandArgumentParser parser = new ScanCommandArgumentParser();
-        List<String> arguments = parser.parse(command);
+        List<String> arguments = parser.parse(argumentsWithSpaces);
         assertTrue(arguments.contains("--exclude"));
         assertTrue(arguments.contains("\"/Users/joe/test folder/\""));
+    }
+
+    @Test
+    public void testReturnsEmpytListWhenNumberOfNonEscapedQuotesIsUneven() {
+        ScanCommandArgumentParser parser = new ScanCommandArgumentParser();
+
+        String command1 = "--exclude \"this\" \"";
+        Assertions.assertEquals(0, parser.parse(command1).size());
+
+        String command2 = "--exclude \"thi\"s \"";
+        Assertions.assertEquals(0, parser.parse(command2).size());
+
+        String command3 = "--exclude \"this\\\" \"";
+        Assertions.assertNotEquals(0, parser.parse(command3).size());
+
+        String command4 = "--exclude \"this\\\" thing\"\"";
+        Assertions.assertEquals(0, parser.parse(command4).size());
+    }
+
+    @Test
+    public void testParsesArgumentsWithEscapedQuotes() {
+        ScanCommandArgumentParser parser = new ScanCommandArgumentParser();
+
+        String command1 = "--exclude \"/Users/joe/test \\\" folder/\"";
+        List<String> arguments1 = parser.parse(command1);
+        assertTrue(arguments1.contains("--exclude"));
+        assertTrue(arguments1.contains("\"/Users/joe/test \\\" folder/\""));
+
+        String command2 = "--exclude \"/Users/joe/tes\\\"t folder/\"";
+        List<String> arguments2 = parser.parse(command2);
+        assertTrue(arguments2.contains("--exclude"));
+        assertTrue(arguments2.contains("\"/Users/joe/tes\\\"t folder/\""));
+
+        String command3 = "--exclude \"/Users/joe/tes\\\\\"t folder/\"";
+        List<String> arguments3 = parser.parse(command3);
+        assertTrue(arguments3.contains("--exclude"));
+        assertTrue(arguments3.contains("\"/Users/joe/tes\\\\\"t folder/\""));
     }
 }
