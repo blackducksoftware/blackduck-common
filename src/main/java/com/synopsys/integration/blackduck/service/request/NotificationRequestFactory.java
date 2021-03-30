d729a9ad2166e1224c102d4b6a80be077f0a4cb1
Gavin Killough <gavink@synopsys.com>
2021-03-30 13:29:01 -0400

    Create factory for notification requests

---
+++ src/main/java/com/synopsys/integration/blackduck/service/request/NotificationRequestFactory.java
@@ -0,0 +1,54 @@
+/*
+ * blackduck-common
+ *
+ * Copyright (c) 2021 Synopsys, Inc.
+ *
+ * Use subject to the terms and conditions of the Synopsys End User Software License and Maintenance Agreement. All rights reserved worldwide.
+ */
+package com.synopsys.integration.blackduck.service.request;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.TimeZone;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import com.synopsys.integration.blackduck.api.manual.temporary.enumeration.NotificationType;
+import com.synopsys.integration.blackduck.http.BlackDuckRequestBuilder;
+import com.synopsys.integration.blackduck.http.BlackDuckRequestFactory;
+import com.synopsys.integration.blackduck.http.BlackDuckRequestFilter;
+import com.synopsys.integration.rest.RestConstants;
+
+public class NotificationRequestFactory {
+    private final List<String> ALL_NOTIFICATION_TYPES = Stream.of(NotificationType.values()).map(Enum::name).collect(Collectors.toList());
+
+    private final BlackDuckRequestFactory blackDuckRequestFactory;
+
+    public NotificationRequestFactory(BlackDuckRequestFactory blackDuckRequestFactory) {
+        this.blackDuckRequestFactory = blackDuckRequestFactory;
+    }
+
+    public BlackDuckRequestBuilder createRequestBuilderForAllNotificationTypes(Date startDate, Date endDate) {
+        return createRequestBuilderForNotificationTypes(startDate, endDate, ALL_NOTIFICATION_TYPES);
+    }
+
+    public BlackDuckRequestBuilder createRequestBuilderForNotificationTypes(Date startDate, Date endDate, List<String> notificationTypesToInclude) {
+        SimpleDateFormat sdf = new SimpleDateFormat(RestConstants.JSON_DATE_FORMAT);
+        sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+        String startDateString = sdf.format(startDate);
+        String endDateString = sdf.format(endDate);
+
+        BlackDuckRequestFilter notificationTypeFilter = createFilterForNotificationsTypes(notificationTypesToInclude);
+        return blackDuckRequestFactory
+                   .createCommonGetRequestBuilder()
+                   .addQueryParameter("startDate", startDateString)
+                   .addQueryParameter("endDate", endDateString)
+                   .addBlackDuckFilter(notificationTypeFilter);
+    }
+
+    private BlackDuckRequestFilter createFilterForNotificationsTypes(List<String> notificationTypesToInclude) {
+        return BlackDuckRequestFilter.createFilterWithMultipleValues("notificationType", notificationTypesToInclude);
+    }
+
+}
