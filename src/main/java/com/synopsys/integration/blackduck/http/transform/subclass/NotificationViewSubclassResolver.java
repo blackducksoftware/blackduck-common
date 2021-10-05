5ff92d817266dd4e72838e82bc966a82a856b785
psantos1113 <psantos@synopsys.com>
2021-10-05 09:58:28 -0400

    feature: Add support for COMPONENT_UNKNOWN_VERSION.

--- src/main/java/com/synopsys/integration/blackduck/http/transform/subclass/NotificationViewSubclassResolver.java
+++ src/main/java/com/synopsys/integration/blackduck/http/transform/subclass/NotificationViewSubclassResolver.java
@@ -11,6 +11,7 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.synopsys.integration.blackduck.api.core.BlackDuckResponse;
 import com.synopsys.integration.blackduck.api.manual.view.BomEditNotificationView;
+import com.synopsys.integration.blackduck.api.manual.view.ComponentUnknownVersionView;
 import com.synopsys.integration.blackduck.api.manual.view.LicenseLimitNotificationView;
 import com.synopsys.integration.blackduck.api.manual.view.NotificationView;
 import com.synopsys.integration.blackduck.api.manual.view.PolicyOverrideNotificationView;
@@ -33,6 +34,8 @@ public class NotificationViewSubclassResolver extends BlackDuckResponseSubclassR
         switch (simpleNotificationView.type) {
             case BOM_EDIT:
                 return BomEditNotificationView.class;
+            case COMPONENT_UNKNOWN_VERSION:
+                return ComponentUnknownVersionView.class;
             case LICENSE_LIMIT:
                 return LicenseLimitNotificationView.class;
             case POLICY_OVERRIDE:
