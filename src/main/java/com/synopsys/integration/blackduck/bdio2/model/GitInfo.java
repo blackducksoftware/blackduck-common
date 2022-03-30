3580a443546afd92757b40025c173a1a6356d87f
Jake Mathews <jake.mathews.email@gmail.com>
2022-03-30 11:41:46 -0400

    style(license): Adding license headers.

--- src/main/java/com/synopsys/integration/blackduck/bdio2/model/GitInfo.java
+++ src/main/java/com/synopsys/integration/blackduck/bdio2/model/GitInfo.java
@@ -1,3 +1,10 @@
+/*
+ * blackduck-common
+ *
+ * Copyright (c) 2022 Synopsys, Inc.
+ *
+ * Use subject to the terms and conditions of the Synopsys End User Software License and Maintenance Agreement. All rights reserved worldwide.
+ */
 package com.synopsys.integration.blackduck.bdio2.model;
 
 import java.net.URL;
