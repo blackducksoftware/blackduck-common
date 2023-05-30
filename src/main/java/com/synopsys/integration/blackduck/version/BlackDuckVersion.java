16377a9ea65c62c781723771a75d7d33aabeff63
blackduck-serv-builder <serv-builder@blackducksoftware.com>
2023-05-30 13:56:27 -0400

    Using the next snapshot post release 66.2.2-SNAPSHOT

--- src/main/java/com/synopsys/integration/blackduck/version/BlackDuckVersion.java
+++ src/main/java/com/synopsys/integration/blackduck/version/BlackDuckVersion.java
@@ -1,3 +1,10 @@
+/*
+ * blackduck-common
+ *
+ * Copyright (c) 2023 Synopsys, Inc.
+ *
+ * Use subject to the terms and conditions of the Synopsys End User Software License and Maintenance Agreement. All rights reserved worldwide.
+ */
 package com.synopsys.integration.blackduck.version;
 
 import com.synopsys.integration.util.Stringable;
