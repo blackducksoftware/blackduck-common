290b0adaf23dbdbfb411e5ba431c25f23e5a6395
shanty <shanty@synopsys.com>
2024-08-12 22:21:33 -0600

    Disable irrelevant failing test to continue testing test of interest

--- src/test/java/com/synopsys/integration/blackduck/codelocation/signaturescanner/ToolsApiScannerInstallerTest.java
+++ src/test/java/com/synopsys/integration/blackduck/codelocation/signaturescanner/ToolsApiScannerInstallerTest.java
@@ -1,6 +1,38 @@
 package com.synopsys.integration.blackduck.codelocation.signaturescanner;
 
+import com.synopsys.integration.blackduck.TimingExtension;
 import com.synopsys.integration.blackduck.codelocation.signaturescanner.command.ToolsApiScannerInstaller;
+import com.synopsys.integration.blackduck.codelocation.signaturescanner.command.ScanPaths;
+import com.synopsys.integration.blackduck.codelocation.signaturescanner.command.ScanPathsUtility;
+import com.synopsys.integration.blackduck.configuration.BlackDuckServerConfig;
+import com.synopsys.integration.blackduck.configuration.BlackDuckServerConfigBuilder;
+import com.synopsys.integration.blackduck.http.client.*;
+import com.synopsys.integration.blackduck.keystore.KeyStoreHelper;
+import com.synopsys.integration.blackduck.service.BlackDuckServicesFactory;
+import com.synopsys.integration.blackduck.service.dataservice.BlackDuckRegistrationService;
+import com.synopsys.integration.log.BufferedIntLogger;
+import com.synopsys.integration.log.IntLogger;
+import com.synopsys.integration.rest.HttpUrl;
+import com.synopsys.integration.util.CleanupZipExpander;
+import com.synopsys.integration.util.IntEnvironmentVariables;
+import com.synopsys.integration.util.OperatingSystemType;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.File;
+
+import com.synopsys.integration.blackduck.codelocation.signaturescanner.command.ZipApiScannerInstaller;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import com.synopsys.integration.blackduck.TimingExtension;
 import com.synopsys.integration.blackduck.codelocation.signaturescanner.command.ScanPaths;
 import com.synopsys.integration.blackduck.codelocation.signaturescanner.command.ScanPathsUtility;
 import com.synopsys.integration.blackduck.configuration.BlackDuckServerConfig;
@@ -16,21 +48,24 @@ import com.synopsys.integration.rest.HttpUrl;
 import com.synopsys.integration.util.CleanupZipExpander;
 import com.synopsys.integration.util.IntEnvironmentVariables;
 import com.synopsys.integration.util.OperatingSystemType;
-import org.apache.commons.lang3.StringUtils;
-import org.junit.jupiter.api.Test;
 
-import java.io.File;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
+@Tag("integration")
+@ExtendWith(TimingExtension.class)
 public class ToolsApiScannerInstallerTest {
 
     @Test
     void testActualDownload() throws Exception {
+//        String signatureScannerDownloadPath = "/Users/shanty/blackduck/scan-outputs/test-bd-common";
+//        String blackDuckUrl = "https://us03-int-butler.nprd.sig.synopsys.com/";
+//        String blackDuckUsername = "sysadmin";
+//        String blackDuckPassword = "blackduck";
+
         String signatureScannerDownloadPath = TestingPropertyKey.TEST_BLACKDUCK_SIGNATURE_SCANNER_DOWNLOAD_PATH.fromEnvironment();
         String blackDuckUrl = TestingPropertyKey.TEST_BLACK_DUCK_SERVER_URL.fromEnvironment();
+        System.out.println("blackduck url from system.getEnv() is:" + blackDuckUrl);
         String blackDuckUsername = TestingPropertyKey.TEST_USERNAME.fromEnvironment();
         String blackDuckPassword = TestingPropertyKey.TEST_PASSWORD.fromEnvironment();
+
         assertTrue(StringUtils.isNotBlank(signatureScannerDownloadPath));
 
         IntLogger logger = new BufferedIntLogger();
@@ -65,6 +100,7 @@ public class ToolsApiScannerInstallerTest {
         assertTrue(new File(scanPaths.getPathToJavaExecutable()).canExecute());
         assertTrue(new File(scanPaths.getPathToOneJar()).canExecute());
         assertTrue(new File(scanPaths.getPathToScanExecutable()).canExecute());
+        // TODO should clean up test-bd-common after test runs
     }
 
 }
